6de9265336413d36616a87469aaafd33372354a9
Shwetas Dhake <shwetas.dhake@mitaoe.ac.in>
2024-07-27 09:56:44 +0530

    code review done

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,12 +1,18 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const loading = () => {
-    return (
-        <section id='spinner'>
-            <Image priority={false} width={135} height={135} src='/images/spinner.gif' alt="spinner" />
-        </section>
-    )
-}
+  return (
+    <section id="spinner">
+      <Image
+        priority={false}
+        width={135}
+        height={135}
+        src="/images/spinner.gif"
+        alt="spinner"
+      />
+    </section>
+  );
+};
 
-export default loading
+export default loading;
