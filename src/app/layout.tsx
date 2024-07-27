de83b7cac4db2298abad75557e7479b1274eb290
Shwetas Dhake <shwetas.dhake@mitaoe.ac.in>
2024-07-27 09:52:33 +0530

    all bugs are fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
 import "../style/style.css";
 import Footer from "@/components/footer/Footer";
-import webLogo from '../../public/images/webLogo.png'
+import webLogo from "../../public/images/webLogo.png";
 import Navbar from "@/components/header/navbar/Navbar";
 import ActiveIcon from "@/components/activeIcon/ActiveIcon";
 
 export const metadata = {
   title: {
-    default: 'Home Page | ShwetDev',
-    template: '%s | ShwetDev'
-  }
-}
+    default: "Home Page | ShwetDev",
+    template: "%s | ShwetDev",
+  },
+};
 
-const RootLayout = ({ children, }: Readonly<{ children: React.ReactNode; }>) => {
+const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <html lang="en">
       <link rel="icon" type="image/png" href={webLogo.src} />
@@ -25,6 +25,6 @@ const RootLayout = ({ children, }: Readonly<{ children: React.ReactNode; }>) => 
       </body>
     </html>
   );
-}
+};
 
-export default RootLayout
+export default RootLayout;
